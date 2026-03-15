fb2cd71a8f9393d07e205c3a0b5e5003c2b6e012
vedangiitb <vedangbaleiitb@gmail.com>
2026-03-15 11:45:00 +0530

    configs update

--- config/env.ts
+++ config/env.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 
+/*
+ * Job context from Worker
+ * SESSION_ID, REQUEST_TYPE & TASKS_PLAN_ID
+ *
+ * Env secrets/variables
+ * SNAPSHOT_BUCKET_NAME, GCP_PROJECT_ID_QWINTLY, ,GEMINI_API_KEY
+ */
+
 export const SESSION_ID = process.env.SESSION_ID!;
+export const TASKS_PLAN_ID = process.env.TASKS_PLAN_ID!;
 export const REQUEST_TYPE = process.env.REQUEST_TYPE!;
-export const REQUEST_BUCKET_NAME =
-  process.env.BUCKET_NAME || "qwintly-builder-requests";
-export const SNAPSHOT_BUCKET_NAME =
-  process.env.SNAPSHOT_BUCKET_NAME || "gen-project-snapshots";
-export const GOOGLE_GENAI_API_KEY = process.env.GOOGLE_GENAI_API_KEY!;
-export const GEN_PROJECT_ID = process.env.GEN_PROJECT_ID || "generated-sites";
-export const INDEX_BUCKET = process.env.INDEX_BUCKET || "qwintly-code-indexes";
+export const SNAPSHOT_BUCKET = process.env.SNAPSHOT_BUCKET;
+export const TEMPLATE_BUCKET = process.env.TEMPLATE_BUCKET;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY!;
+export const GCP_PROJECT_ID_QWINTLY = process.env.GCP_PROJECT_ID_QWINTLY;
